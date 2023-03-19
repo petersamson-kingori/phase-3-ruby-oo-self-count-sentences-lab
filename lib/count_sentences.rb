eb1b6c38f40f5911dfdb41456bc91379b4c12a39
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-03-19 20:15:15 +0300

    Completed Lab

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,27 @@
 require 'pry'
 
-class String
+class Cat
+  attr_accessor :name
 
+  def meow
+    "meow!"
+  end
+end
+
+class String
   def sentence?
-    
+    self.strip.end_with?('.')
   end
 
   def question?
-
+    self.strip.end_with?('?')
   end
 
   def exclamation?
-
+    self.strip.end_with?('!')
   end
 
   def count_sentences
-
+    self.split(/[.?!]+/).reject(&:empty?).count
   end
 end
